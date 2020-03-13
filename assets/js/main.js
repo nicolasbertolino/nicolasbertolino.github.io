7070bfd3f8fcc24bb5320b85d883f8ba2293b9de
Nicolas Bertolino <bertolinonicolas@gmail.com>
2020-03-13 07:58:34 +0100

    Add in projects' modals

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,133 @@
-// Header
+let link = document.getElementsByClassName("project__link");
+
+for (let i = 0; i < link.length; i++) {
+  let thisLink = link[i];
+  thisLink.addEventListener(
+    "click",
+    function() {
+      let project = document.getElementById(this.dataset.project);
+      let overlay = document.querySelector(".overlay");
+      let close = document.querySelector(".close__container");
+      project.classList.add("show-project");
+      overlay.classList.add("show");
+      close.classList.add("show");
+      document.body.classList.add("lock");
+      gsap.fromTo(
+        ".project__infos div:first-child",
+        {
+          y: 20,
+          opacity: 0,
+          ease: "power2"
+        },
+        {
+          delay: 0.7,
+          y: 0,
+          opacity: 1,
+          ease: "power2"
+        }
+      );
+      gsap.fromTo(
+        ".project__infos h4",
+        {
+          y: 20,
+          opacity: 0,
+          ease: "power2"
+        },
+        {
+          delay: 0.8,
+          y: 0,
+          opacity: 1,
+          ease: "power2"
+        }
+      );
+      gsap.fromTo(
+        ".project__infos h2",
+        {
+          y: 20,
+          opacity: 0,
+          ease: "power2"
+        },
+        {
+          delay: 0.9,
+          y: 0,
+          opacity: 1,
+          ease: "power2"
+        }
+      );
+      gsap.fromTo(
+        ".project__description",
+        {
+          y: 20,
+          opacity: 0,
+          ease: "power2"
+        },
+        {
+          delay: 1,
+          y: 0,
+          opacity: 1,
+          ease: "power2"
+        }
+      );
+      gsap.fromTo(
+        ".project__see",
+        {
+          y: 20,
+          opacity: 0,
+          ease: "power2"
+        },
+        {
+          delay: 1.1,
+          y: 0,
+          opacity: 1,
+          ease: "power2"
+        }
+      );
+      gsap.fromTo(
+        ".project__number",
+        {
+          y: 20,
+          opacity: 0,
+          ease: "power2"
+        },
+        {
+          delay: 1.2,
+          y: 0,
+          opacity: 1,
+          ease: "power2"
+        }
+      );
+      close.addEventListener("click", function() {
+        overlay.classList.remove("show");
+        close.classList.remove("show");
+        project.classList.remove("show-project");
+        setTimeout(function() {
+          document.body.classList.remove("lock");
+        }, 800);
+        gsap.to(".project__infos div:first-child", {
+          opacity: 0,
+        });
+        gsap.to(".project__infos h4", {
+          opacity: 0,
+        });
+        gsap.to(".project__infos h2", {
+          opacity: 0,
+        });
+        gsap.to(".project__description", {
+          opacity: 0,
+        });
+        gsap.to(".project__see", {
+          opacity: 0,
+        });
+        gsap.to(".project__number", {
+          opacity: 0,
+        });
+      });
+    },
+    false
+  );
+}
+
+// Header GSAP
 gsap.from(".name", {
   duration: 1,
   delay: 1.5,
@@ -63,7 +192,7 @@ gsap.from(".socials li", {
 // ScrollMagic Scenes
 let controller = new ScrollMagic.Controller();
 
-let projects = gsap.from(".projects div a", {
+let projects = gsap.from(".projects div p", {
   duration: 1,
   y: "100%",
   opacity: 0,
@@ -88,8 +217,10 @@ function selectedImg(mql) {
 
     let scene2 = new ScrollMagic.Scene({
       triggerElement: ".selected-work",
-      triggerHook: 0.5
-    });
+      triggerHook: 0.8
+    })
+      .setTween(selected__img)
+      .addTo(controller);
   } else {
     let selected__img = gsap.from(".img img", {
       duration: 2,
@@ -100,7 +231,9 @@ function selectedImg(mql) {
     let scene2 = new ScrollMagic.Scene({
       triggerElement: ".selected-work",
       triggerHook: 0.5
-    });
+    })
+      .setTween(selected__img)
+      .addTo(controller);
   }
 }
 
@@ -136,19 +269,41 @@ let scene4 = new ScrollMagic.Scene({
   .setTween(right__div)
   .addTo(controller);
 
-let catchphrase = gsap.from(".catchphrase h3", {
-  duration: 1,
-  scale: 1.2,
-  opacity: 0,
-  ease: "power4"
-});
+function selectedCatchphrase(mql2) {
+  if (mql2.matches) {
+    let catchphrase = gsap.from(".catchphrase h3", {
+      duration: 1,
+      scale: 1,
+      y: 50,
+      opacity: 0,
+      ease: "power4"
+    });
 
-let scene5 = new ScrollMagic.Scene({
-  triggerElement: ".catchphrase",
-  triggerHook: 0.7
-})
-  .setTween(catchphrase)
-  .addTo(controller);
+    let scene5 = new ScrollMagic.Scene({
+      triggerElement: ".catchphrase",
+      triggerHook: 0.7
+    })
+      .setTween(catchphrase)
+      .addTo(controller);
+  } else {
+    let catchphrase = gsap.from(".catchphrase h3", {
+      duration: 1,
+      scale: 1.2,
+      opacity: 0,
+      ease: "power4"
+    });
+
+    let scene5 = new ScrollMagic.Scene({
+      triggerElement: ".catchphrase",
+      triggerHook: 0.7
+    })
+      .setTween(catchphrase)
+      .addTo(controller);
+  }
+}
+
+let mql2 = window.matchMedia("(max-width: 834px)");
+selectedCatchphrase(mql2);
 
 let about = gsap.from(".about div *", {
   duration: 1,
